f955693df0e3abbbfba56392385080a815828adc
jakeols <jake_ols@live.com>
2019-09-27 19:42:45 -0700

    vectors

--- src/main.rs
+++ src/main.rs
@@ -1,157 +1,87 @@
-// note: Rust will not dynamically allocate memory for the node at runtime, must wrap it / be explicit
-// note: use SHA-512 for hash of val
-// key is hash of the JSON string of the entry and value is the JSON string of the entry.
+/**
+ * Vector implementation with vec and hashmap of binary merkle tree
+ */
 
-// -------------------------------------------------------------------------------------
-// -------------------------------------------------------------------------------------
-// -------------------------------------------------------------------------------------
-// ---------------------------------START-----------------------------------------------
+use std::collections::HashMap;
 
-//use sha2::{Sha512, Digest}; // use sha2 crate 
-extern crate crypto;
-#[macro_use] extern crate hex_literal;
-use self::crypto::digest::Digest;
-use self::crypto::sha2::Sha512; // use crypto lib for this bc of string implementation
-use std::str;
-
-
-// root hash, i think this is necessary
-
-
-// define the struct for our node
 #[derive(Debug, Clone)]
-struct Node<'a> {
-    hash: String, // the node's hash
-    val: &'a str, // JSON string of the nodes value
-    l: Option<Box<Node<'a>>>, // can use box / rc etc
-    r: Option<Box<Node<'a>>>,
+struct Node {
+    hash: String,
+    value: String,
 }
 
+#[derive(Debug, Clone)]
+struct Tree {
+    nodes: Vec<Node>,
+    leaf_nodes: u128,
+}
 
-
-impl<'a> Node<'a> {
-
-    //  insert(key, value), @return string (new root hash)
-  pub fn insert(&mut self, key: &'a str, new_val: &'a str) {
-    
-    let test_hash : String = calculate_root_hash(self.val.as_bytes(), new_val.as_bytes());
-
-    // create new root
-    let mut new_root_node = Node {hash: "".to_string(), val: "", l: None, r: None};
-
-    // create new leaf
-    let new_leaf_node = Node { hash: key.to_string(), val: new_val, l: None, r: None};
-    let new_boxed_leaf_node = Some(Box::new(new_leaf_node));
-    
-    // set root nodes
-    new_root_node.r = new_boxed_leaf_node;
-    new_root_node.l = Some(Box::new(self.clone()));
-
-    new_root_node.hash = test_hash;
-
-    // re-assign x to our new tree
-    *self = new_root_node;
-
+impl Node {
+    pub fn get_hash(self) -> String {
+        return self.hash;
     }
 
-    // generateMerklePath @param key, @return vector 
-    pub fn generate_merkle_path(&mut self, key: &'a str) -> Vec<Node> {
-        let mut vec = Vec::new();
-        let mut path = Vec::new();
+    pub fn get_value(self) -> String {
+        return self.value;
+    }
+}
 
-        let mut temp_self = self.clone();
+impl Tree {
 
-        // vector rep
-        loop {
-             if temp_self.l.is_none() == true || temp_self.l.is_none() == true {
-                 break;
-            }
+    pub fn insert(&mut self, node: Node) {
+        self.nodes.push(node);
+        self.leaf_nodes = self.leaf_nodes + 1;
 
-            else {
-                
-            vec.push(temp_self.l.clone());
-            temp_self.l = None;
+        // create a hash 
 
-            vec.push(temp_self.r.clone());
-            temp_self.r = None;
-          //  println!("{:?}", vec);
+        let new_hash = Node {hash: "hash".to_string(), value: "val".to_string()};
+        self.nodes.push(new_hash);
+    }
+
+    pub fn delete(&mut self, key: String){
+        let v = &mut self.nodes;
+
+        // find the node to remove
+        for i in 0..v.len() - 1 {
+            if v[i].hash == key {
+                v.swap_remove(i);
             }
         }
 
+        // new remove hashes   
 
-        
-
-        return path;
-        
     }
 
- pub fn delete(&mut self, key: &'a str) -> bool {
-     
+    pub fn calculate_length(mut self) -> usize{
 
-	if key == self.hash {
-			return true;
-		}
+        let v = &mut self.nodes;
 
-        match self.r {
-            None => false,
-            Some(ref mut node) => node.delete(key)
+        let mut len = 0; 
+        for i in 0..v.len(){
+            len += i;
         }
-		
-		// else {
-		// 	match self.val {
-		// 		None => false,
-		// 		Some(ref mut node) => node.delete(key)
-		// 	}
-		// }
-        
-	
+        return len;
 
+    }
 
-}
-
-// pub fn verify_merkle_path(&mut self, Vec<Node> merkle_path){
-
-// }
-
-
-
-}
-
-
-
-pub fn hash_string(val: &[u8]) -> String{
-    let mut hasher = Sha512::new();
-
-    hasher.input(val);
-    let hashed = hasher.result_str();
-
-    return hashed;
-}
-
-pub fn calculate_root_hash<'a>(first: &[u8], second: &[u8]) -> String {
-    let mut hasher = Sha512::new();
-
-    hasher.input(first);
-    hasher.input(second);
-    let root_hash : String = hasher.result_str();
-
-    return root_hash;
-}
-
-fn main () {
-    let mut x = Node {hash: hash_string(b"mkey").to_string(), val: "m", l: None, r: None };
-    
-    let hashed = hash_string(b"z");
-    x.insert(&hashed, "z");
-
-    let hashed_two = hash_string(b"k");
-    x.insert(&hashed_two, "k");
    
-   println!("{:?}", x);
-   println!("deleting...");
-   let key_to_del = "2af8a9104b3f64ed640d8c7e298d2d480f03a3610cbc2b33474321ec59024a48592ea8545e41e09d5d1108759df48ede0054f225df39d4f0f312450e0aa9dd25";
-   x.delete(key_to_del);
-   println!("{:?}", x);
+}
 
-   x.generate_merkle_path("z");
+fn main() {
+    let root = Node {hash: "teshhash".to_string(), value: "test value".to_string()};
+    let merkle_nodes = Vec::new();
+
+    let mut MerkleTree = Tree {leaf_nodes: 0, nodes: merkle_nodes};
+    MerkleTree.insert(root);
+    let new_leaf = Node {hash: "teshhashtwo".to_string(), value: "tesvaltwo".to_string()};
+    MerkleTree.insert(new_leaf);
+
+    println!("{:?}", MerkleTree);
+
+    MerkleTree.delete("teshhashtwo".to_string());
+
+    println!("{:?}", MerkleTree);
+
+    println!("{}", MerkleTree.calculate_length());
+
 }
